cd334ac852ba9debe2558d0a76c0d6791e7ef5f7
Weberson Vansovski <vansovskiPYTHON@gmail.com>
2022-11-03 09:33:01 -0400

    segredos.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from secretkey import segredos
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '3bcd4ce9217c7b0aba1c607fd04ea000'
+app.config['SECRET_KEY'] = segredos.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
---
+++ comunidadeimpressionadora/secretkey.py
@@ -0,0 +1,3 @@
+segredos = {
+'SECRET_KEY': '3bcd4ce9217c7b0aba1c607fd04ea000'
+}
